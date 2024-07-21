c87a298efbd60a0ad7af38058008197e73003a71
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-22 00:28:37 +0300

    Modified mOdels.py

--- server/models.py
+++ server/models.py
@@ -14,3 +14,12 @@ class Item(db.Model):
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
     reviews = db.relationship('Review', back_populates='item')
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String(200), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
