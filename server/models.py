687066c52ea67cb9f7f8640fc828a9d4f74f714d
SirCartermme <samuel.maina1@student.moringaschool.comm>
2024-07-22 00:27:53 +0300

    Modified mOdels.py

---
+++ server/models.py
@@ -0,0 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Customer(db.Model):
+    __tablename__ = 'customers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    reviews = db.relationship('Review', back_populates='customer')
